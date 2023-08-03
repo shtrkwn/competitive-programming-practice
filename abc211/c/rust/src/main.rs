5c8f3a087f48d4ae62196484b2ef52fd6bc42d2d
Shotaro Kawano <shotaro.kawano.dev@gmail.com>
2023-08-03 19:44:48 +0900

    模範解答を見て修正：計算量を削減するアルゴリズムに変更（O(N^2) -> O(N)）

--- abc211/c/rust/src/main.rs
+++ abc211/c/rust/src/main.rs
@@ -5,26 +5,23 @@ fn main() {
     s: String
   };
 
-  let s_chars: Vec<char> = s.chars().collect();
-  let n = s_chars.len();
-  let mut dp: Vec<Vec<usize>> = vec![vec![0; s.len()]; 8];
+  let mut dp: Vec<usize> = vec![0; 8];
   let pattern = ['c', 'h', 'o', 'k', 'u', 'd', 'a', 'i'];
   let div: usize = 1000000007;
 
-  // 初期化
-  for j in 0..n {
-    if s_chars[j] == 'c' {
-      dp[0][j] = 1;
-    }
-  }
-
-  for i in 1..8 {
-    for j in 0..n {
-      if s_chars[j] == pattern[i] {
-        dp[i][j] = dp[i - 1][0..j].iter().fold(0, |acc, x| acc + x) % div;
+  for c in s.chars() {
+    match pattern.iter().position(|p| p == &c) {
+      Some(i) => {
+        if i == 0 {
+          dp[0] += 1;
+        } else {
+          dp[i] = dp[i] + dp[i - 1];
+        }
+        dp[i] %= div;
       }
-    }
+      None => {}
+    };
   }
 
-  println!("{}", dp[7].iter().fold(0, |acc, &x| acc + x) % div);
+  println!("{}", dp[7]);
 }
