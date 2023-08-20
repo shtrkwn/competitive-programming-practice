3b489942faa69f086dd335e613f65a431f3ddd54
Shotaro Kawano <shotaro.kawano.dev@gmail.com>
2023-08-20 22:54:02 +0900

    abc315_e

--- abc315/e/rust/src/main.rs
+++ abc315/e/rust/src/main.rs
@@ -1,5 +1,4 @@
 use proconio::*;
-use std::collections::HashSet;
 
 fn main() {
   input! {
@@ -12,63 +11,73 @@ fn main() {
       c: usize,
       p: [usize; c]
     }
-    // 補正
     graph.push(p.into_iter().map(|x| x - 1).collect());
   }
 
-  let mut done_books: HashSet<usize> = HashSet::new();
-  let mut order: Vec<usize> = Vec::new();
-  let mut history: Vec<usize> = Vec::new();
-
-  find_basic_book_and_read_route(&1, &graph, &done_books, &mut order);
-  read_from_basics(order, &graph, &mut done_books, &mut history);
-  println!("{:?}", history);
-}
-
-fn find_basic_book_and_read_route(
-  target_book: &usize,
-  graph: &Vec<Vec<usize>>,
-  done_books: &HashSet<usize>,
-  order: &mut Vec<usize>,
-) {
-  order.push(*target_book);
-  let nexts = &graph[*target_book];
-  for next in nexts {
-    if !done_books.contains(&next) {
-      find_basic_book_and_read_route(&next, &graph, done_books, order);
-      break;
+  match topological_sort(&graph, 0) {
+    Some(order) => {
+      for el in order.iter().rev() {
+        if el == &0 {
+          break;
+        }
+        print!("{} ", el + 1);
+      }
+      println!();
     }
+    None => println!("The graph is not a DAG!"),
   }
 }
+fn dfs(graph: &Vec<Vec<usize>>, start: usize, visited: &mut Vec<bool>) -> Vec<usize> {
+  let mut stack = vec![start];
+  let mut component = vec![];
 
-fn read_from_basics(
-  order: Vec<usize>,
-  graph: &Vec<Vec<usize>>,
-  done_books: &mut HashSet<usize>,
-  history: &mut Vec<usize>,
-) {
-  // この関数の前提：読むことが可能な本を入力する（この関数でよんでよいかの判定はしない、入力されたものは読んでいいものとする）
-  // 本を読んだら、その上位の本を読むこと試みる
-  // 前提となる本をすべて読んでいれば、その本を同様に読む
-  // 前提となる本で未読のものがあれば、前提となるルートの本を探して同じく読む
-
-  // 本を読む
-  for &next in order.iter().rev() {
-    let mut ready = true;
-    for prerequisit in graph[next].iter() {
-      if !done_books.contains(&prerequisit) {
-        ready = false;
-        break;
+  while let Some(node) = stack.pop() {
+    if !visited[node] {
+      visited[node] = true;
+      component.push(node);
+      for &next in &graph[node] {
+        if !visited[next] {
+          stack.push(next);
+        }
       }
     }
+  }
 
-    if ready {
-      history.push(next + 1);
-      done_books.insert(next);
-    } else {
-      let mut order: Vec<usize> = Vec::new();
-      find_basic_book_and_read_route(&next, graph, done_books, &mut order);
-      read_from_basics(order, graph, done_books, history);
+  component
+}
+
+fn topological_sort(graph: &Vec<Vec<usize>>, start: usize) -> Option<Vec<usize>> {
+  let mut visited = vec![false; graph.len()];
+  let component = dfs(graph, start, &mut visited);
+
+  let mut indegree = vec![0; graph.len()];
+
+  for &node in &component {
+    for &next in &graph[node] {
+      indegree[next] += 1;
     }
+  }
+
+  let mut queue: Vec<usize> = component
+    .clone()
+    .into_iter()
+    .filter(|&i| indegree[i] == 0)
+    .collect();
+  let mut sorted = Vec::new();
+
+  while let Some(node) = queue.pop() {
+    sorted.push(node);
+    for &next in &graph[node] {
+      indegree[next] -= 1;
+      if indegree[next] == 0 {
+        queue.push(next);
+      }
+    }
+  }
+
+  if sorted.len() == component.len() {
+    Some(sorted)
+  } else {
+    None
   }
 }
