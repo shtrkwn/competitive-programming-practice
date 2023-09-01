25e2b8f126527ed81de210395b5dca01d8f4b52d
Shotaro Kawano <shotaro.kawano.dev@gmail.com>
2023-09-02 00:10:54 +0900

    refactored abc211_d

--- abc211/d/rust/src/main.rs
+++ abc211/d/rust/src/main.rs
@@ -6,54 +6,58 @@ struct Graph {
 }
 
 impl Graph {
+  // 新しいグラフを初期化
   fn new(n: usize) -> Self {
     Graph {
       adj_list: vec![vec![]; n],
     }
   }
 
+  // グラフにエッジを追加
   fn add_edge(&mut self, u: usize, v: usize) {
     self.adj_list[u].push(v);
   }
 
+  // 幅優先探索を使用して、スタートからの最短距離と各ノードへのルート数を計算
   fn bfs(&self, start: usize) {
     let n = self.adj_list.len();
     let mut visited = vec![false; n];
-    let mut shortes_arrivals = vec![0; n];
+    let mut distance_from_start = vec![0; n];
     let mut num_routes = vec![0; n];
     let mut queue = VecDeque::new();
     visited[start] = true;
-    shortes_arrivals[start] = 0;
+    distance_from_start[start] = 0;
     num_routes[start] = 1;
     queue.push_back(start);
 
     while let Some(current) = queue.pop_front() {
       for &neighbour in &self.adj_list[current] {
-        if !visited[neighbour] {
+        if !visited[neighbour] || distance_from_start[neighbour] == distance_from_start[current] + 1
+        {
+          if !visited[neighbour] {
+            queue.push_back(neighbour);
+          }
           visited[neighbour] = true;
-          shortes_arrivals[neighbour] = shortes_arrivals[current] + 1;
-          num_routes[neighbour] += num_routes[current];
-          queue.push_back(neighbour);
-        } else if shortes_arrivals[neighbour] == shortes_arrivals[current] + 1 {
-          num_routes[neighbour] += num_routes[current];
+          distance_from_start[neighbour] = distance_from_start[current] + 1;
+          num_routes[neighbour] = (num_routes[neighbour] + num_routes[current]) % 1_000_000_007;
         }
       }
     }
-    // println!("{:?}", num_routes);
-    println!("{}", num_routes[n - 1]);
+
+    let destination = n - 1;
+    println!("{}", num_routes[destination]);
   }
 }
 
 fn main() {
   input! {
-    n:usize,
-    m:usize,
-    roads:[(usize, usize);m]
+      n:usize,
+      m:usize,
+      roads:[(usize, usize); m]
   }
 
   let mut graph = Graph::new(n);
 
-  // citiesの初期化
   for (city1, city2) in roads {
     graph.add_edge(city1 - 1, city2 - 1);
     graph.add_edge(city2 - 1, city1 - 1);
