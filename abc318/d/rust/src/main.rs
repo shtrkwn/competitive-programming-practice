e936ab8dda710a272d921aa46b55f92267058240
Shotaro Kawano <shotaro.kawano.dev@gmail.com>
2023-09-05 03:20:00 +0900

    RTEを回避するためにロジックを修正

--- abc318/d/rust/src/main.rs
+++ abc318/d/rust/src/main.rs
@@ -1,63 +1,54 @@
 use proconio::*;
 
-#[derive(Clone, Debug)]
-struct EdgeGroup {
-  dest: usize,
-  weight: usize,
-}
-
 fn main() {
   input! {
-    n:usize,
+    mut n:usize,
     d:[usize;n*(n-1)/2]
   }
 
   let mut edges: Vec<Vec<usize>> = vec![vec![]; n - 1];
-  let mut counter = n;
+  let mut counter = n - 1;
   let mut node_index = 0;
   for i in 0..d.len() {
     edges[node_index].push(d[i]);
     counter -= 1;
     if counter == 0 {
-      counter = n - node_index - 1;
+      counter = (n - 1) - node_index - 1;
       node_index += 1;
     }
   }
 
-  // EdgeGroupに入れ替え
-  let mut edge_groups: Vec<Vec<EdgeGroup>> = vec![vec![]; n - 1];
-  for i in 0..(n - 1) {
-    for j in 0..edges[i].len() {
-      edge_groups[i].push(EdgeGroup {
-        dest: j + i,
-        weight: edges[i][j],
-      })
+  // nが奇数の場合使わないノードを可変にするために、重み0のノードを追加
+  if n % 2 == 1 {
+    n += 1;
+    for i in 0..edges.len() {
+      edges[i].push(0);
     }
-    edge_groups[i].sort_by(|x1, x2| x2.weight.cmp(&x1.weight));
+    edges.push(vec![0]);
   }
-
-  let ans = dfs(&edge_groups, &mut vec![false; n]);
+  let ans = dfs(&edges, &mut vec![false; n], 0);
   println!("{}", ans);
 }
 
-fn dfs(edge_groups: &Vec<Vec<EdgeGroup>>, used_node: &mut Vec<bool>) -> usize {
+fn dfs(edges: &Vec<Vec<usize>>, used_node: &mut Vec<bool>, start: usize) -> usize {
   let mut max = 0;
-  for i in 0..edge_groups.len() {
-    if used_node[i] {
+  for node1_id in start..edges.len() {
+    if used_node[node1_id] {
       continue;
     }
-    used_node[i] = true;
+    used_node[node1_id] = true;
 
-    for j in 0..edge_groups[i].len() {
-      if !used_node[edge_groups[i][j].dest] {
-        used_node[edge_groups[i][j].dest] = true;
-        // println!("{:?}", used_node);
-        max = (edge_groups[i][j].weight + dfs(edge_groups, used_node)).max(max);
-        used_node[edge_groups[i][j].dest] = false;
+    for j in 0..edges[node1_id].len() {
+      let node2_id = node1_id + 1 + j;
+      if used_node[node2_id] {
         continue;
       }
+
+      used_node[node2_id] = true;
+      max = (edges[node1_id][j] + dfs(edges, used_node, node1_id + 1)).max(max);
+      used_node[node2_id] = false;
     }
-    used_node[i] = false;
+    used_node[node1_id] = false;
   }
 
   return max;
